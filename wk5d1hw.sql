936c2f8564961f86589f8ea8486711311a020749
adonaione <adonairomero@icloud.com>
2024-03-19 09:28:16 -0600

    fixed

--- wk5d1hw.sql
+++ wk5d1hw.sql
@@ -2,60 +2,69 @@
 
 SELECT count(last_name)
 FROM actor a 
-WHERE last_name LIKE 'Wahlberg';
+WHERE last_name = 'Wahlberg';
 
 
 -- 2. How many payments were made between $3.99 and $5.99?
 
---NEED HELP
 SELECT count(payment_id) 
-FROM payment
-GROUP BY payment_id  
-HAVING amount BETWEEN 3.99 AND 5.99;
+FROM payment  
+where amount BETWEEN 3.99 AND 5.99;
+
 
 -- 3. What films have exactly 7 copies? (search in inventory)
-SELECT inventory_id, title
-FROM inventory i, film f 
-WHERE inventory_id = 7;
+
+SELECT film_id, count(*) AS num_films
+FROM inventory i 
+GROUP BY film_id 
+HAVING count(*) = 7;
 
 -- 4. How many customers have the first name ‘Willie’?
 
-SELECT *
+SELECT count(*)
 FROM customer 
 WHERE first_name LIKE 'Willie';
 
+
 -- 5. What store employee (get the id) sold the most rentals (use the rental table)?
 
-SELECT staff_id, count(staff_id)
+SELECT staff_id, count(staff_id) AS num_rentals
 FROM rental r
-GROUP BY staff_id;
+GROUP BY staff_id
+ORDER BY num_rentals DESC;
+
 
 -- 6. How many unique district names(states) are there? (address table)
 
-SELECT district 
-FROM address
-GROUP BY district;
+SELECT count(DISTINCT district) 
+FROM address;
 
--- show me the count of the unique names
-;
 
 -- 7. What film has the most actors in it? (use film_actor table and get film_id)
 
-SELECT film_id 
-FROM film_actor fa;
+SELECT film_id, count(*)
+FROM film_actor fa
+GROUP BY film_id
+ORDER BY count(*) DESC
+LIMIT 1;
+
 
 -- 8. From store_id 1, how many customers have a last name ending with ‘es’? (use customer table)
 
 SELECT count(*)
 FROM customer c 
-WHERE last_name LIKE '%es';
+WHERE store_id = 1 AND last_name LIKE '%es';
+
 
 -- 9. How many payment amounts (4.99, 5.99, etc.) had a number of rentals above 250 for customers
 -- with ids between 380 and 430? (use group by and having > 250)
 
-SELECT 
+SELECT amount, count(*)
 FROM payment p 
-GROUP BY 
+WHERE customer_id BETWEEN 380 AND 430
+GROUP BY amount 
+HAVING count(*) > 250;
+
 
 -- 10. Within the film table, how many rating categories are there? And what rating has the most
 -- movies total?
@@ -64,3 +73,4 @@ SELECT count(rating), rating
 FROM film f
 GROUP BY rating
 ORDER BY count(rating) DESC;
+
